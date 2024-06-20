e8f658860857e8638115439c8c2f79337e03136a
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-06-20 07:28:18 +0100

    added query service

---
+++ query/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Parse JSON bodies
+app.use(express.json({ limit: "10kb" }));
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(cors());
+
+const posts = {};
+
+app.get("/events", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
